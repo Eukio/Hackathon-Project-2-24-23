ba43d34997983dae5e37bc15669daae832a4aa97
eukio <eukio@Eukio_T.localdomain>
2023-02-25 16:58:30 -0600

    Worked in Game and Term

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,39 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Player player;
+    bool isPlaying;
+    List<Term> termlist = new List<Term>();
+    int correctQuestions;
+    float finalscore;
+    long startTime;
+    long elapsed;
+
+
     void Start()
     {
-        //Yes?
+        elapsed = 0;
+        startTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlaying)
+        {
+            elapsed = (DateTime.Now.Ticks - startTime) / 10000;
+        }
+    }
+    public void RestartTime()
+    {
+        startTime = DateTime.Now.Ticks;
+        elapsed = 0;
+    }
+    public void ReadInput(string s)
+    { //seperate this 
+        string question = "";
+        question = s;
+        string answer = "";
+        answer = s;
     }
 }
--- Assets/Scripts/Term.cs
+++ Assets/Scripts/Term.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Terms : MonoBehaviour
+public class Term : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string question { get; set; }
+    public string answer { get; set; }
     void Start()
     {
 
