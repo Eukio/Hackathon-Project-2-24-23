075e53ed0e8931819c8b5e7cc30241237d798dd9
Mixer <mixer4alife@gmail.com>
2023-02-25 10:34:27 -0600

    fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        int health;
-        int attack;
-        float speed;
-        Rigidbody rb;//asfasdfds
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,11 +6,10 @@ public class Enemy : MonoBehaviour
 {
     public int health;
     public int attack;
-    
-    [SerializeField] float speed;
-    
-    private Rigidbody rb; 
 
+    [SerializeField] float speed;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,4 +22,3 @@ public class Enemy : MonoBehaviour
         
     }
 }
->>>>>>> 61f0c1c (Commit message)
