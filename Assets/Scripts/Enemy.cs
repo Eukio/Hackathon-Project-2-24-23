7e64cdb79ce1cd063967762e1ccbbcf6a65332c2
Mixer <mixer4alife@gmail.com>
2023-02-25 10:13:09 -0600

    shgfkj

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -27,13 +27,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int health;
+    public int attack;
+    
+    [SerializeField] float speed;
+    
+    private Rigidbody rb; 
+
     // Start is called before the first frame update
     void Start()
     {
-        int health;
-        int attack;
-        float speed;
-        Rigidbody rb;//asfasdfds
+
     }
 
     // Update is called once per frame
