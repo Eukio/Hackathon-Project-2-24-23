200de46104a83fe1b6fb117ccc9773c4166ff588
Eukio <82066055+Eukio@users.noreply.github.com>
2023-02-25 22:02:32 -0600

    Update Term.cs

--- Assets/Scripts/Term.cs
+++ Assets/Scripts/Term.cs
@@ -1,18 +1,12 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Terms : MonoBehaviour
+public class Term : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        string s;//asdfadsf
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string question { get; set; }
+    public string answer { get; set; }
+  
 }
